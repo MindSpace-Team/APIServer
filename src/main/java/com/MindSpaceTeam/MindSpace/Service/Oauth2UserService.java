2334758f1bc9fd9991cf5587f94fd9cd45e887b1
Younggwan <kimyogun78@naver.com>
2025-06-09 15:14:57 +0900

    feat: Oauth2 사용자 검증 및 인증을 위한 Service구현 - 사용자 검증 및 인증 결과를 반환하기 위한 LoginResult Enum타입 정의 - 사용자 검증 및 인증 로직을 수행하는 Oauth2UserService구현

--- src/main/java/com/MindSpaceTeam/MindSpace/Service/Oauth2UserService.java
+++ src/main/java/com/MindSpaceTeam/MindSpace/Service/Oauth2UserService.java
@@ -1,35 +1,69 @@
 package com.MindSpaceTeam.MindSpace.Service;
 
 import com.MindSpaceTeam.MindSpace.Repository.UserRepository;
-import com.MindSpaceTeam.MindSpace.dto.Users;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import com.MindSpaceTeam.MindSpace.Entity.Users;
+import com.MindSpaceTeam.MindSpace.Service.Result.LoginResult;
+import com.MindSpaceTeam.MindSpace.dto.EntityConverter;
+import com.MindSpaceTeam.MindSpace.dto.GoogleUserInfoDto;
+import com.MindSpaceTeam.MindSpace.dto.UserInfoDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
 import java.util.Optional;
 
 @Service
-public class Oauth2UserService extends DefaultOAuth2UserService {
+public class Oauth2UserService {
     private UserRepository userRepository;
+    private EntityConverter entityConverter;
+    @Value("spring.security.oauth2.client.registration.google.client-secret")
+    private String clientSecret;
 
-    public Oauth2UserService(UserRepository userRepository) {
+    public Oauth2UserService(UserRepository userRepository, EntityConverter entityConverter) {
         this.userRepository = userRepository;
+        this.entityConverter = entityConverter;
     }
 
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        Users user = Users.builder().email(oAuth2User.getAttribute("email"))
-                        .name(oAuth2User.getAttribute("name"))
-                                .oauthProvider("google")
-                                        .role("USER").build();
-        Optional<Users> foundedUser = this.userRepository.findByEmail(user.getEmail());
-        if (foundedUser.isEmpty()) {
-            this.userRepository.save(user);
-            this.userRepository.flush();
+    // Todo: Refactoring필요
+    public LoginResult processLogin(String bodyData) {
+        try {
+            String userInfoJwtToken = getProfileToken(bodyData);
+            String userInfo = parseProfileJWTToken(userInfoJwtToken);
+            // DTO변환
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode nodes = mapper.readTree(userInfo);
+            UserInfoDto userDto = GoogleUserInfoDto.builder()
+                    .email(nodes.get("email").asText())
+                    .name(nodes.get("name").asText())
+                    .oauthProvider("google")
+                    .role("USER").build();
+            Users user = this.entityConverter.getUserEntity(userDto);
+            Optional<Users> foundedUser = this.userRepository.findByEmail(user.getEmail());
+            if (foundedUser.isEmpty()) {
+                this.userRepository.save(user);
+                return LoginResult.SIGN_UP_SUCCESS;
+            } else {
+                return LoginResult.LOGIN_SUCCESS;
+            }
+        } catch(JsonProcessingException e) {
+            return LoginResult.SERVER_ERROR;
         }
-        return super.loadUser(userRequest);
+    }
+
+    public String getProfileToken(String bodyData) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(bodyData);
+        return node.get("id_token").asText();
+    }
+
+    public String parseProfileJWTToken(String jwtToken) {
+        String[] sections = jwtToken.split("\\.");
+        String payload = sections[1];
+        Base64.Decoder decoder = Base64.getDecoder();
+        String infos = new String(decoder.decode(payload));
+        return infos;
     }
 }
