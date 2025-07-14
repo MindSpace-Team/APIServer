3006258f0d874074dbed9371cc3ad609eecd4c1b
Younggwan <kimyogun78@naver.com>
2025-07-14 23:11:27 +0900

    config: Redis Session활용 설정

--- src/main/java/com/MindSpaceTeam/MindSpace/Config/RedisConfig.java
+++ src/main/java/com/MindSpaceTeam/MindSpace/Config/RedisConfig.java
@@ -7,8 +7,10 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
+@EnableRedisHttpSession
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
