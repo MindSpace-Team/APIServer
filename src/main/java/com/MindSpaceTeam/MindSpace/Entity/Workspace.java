f4d7062e223ef9071a2ab608b82241e4cfb7f7a6
김영관 <kimyogun78@naver.com>
2025-07-22 15:07:51 +0900

    refactor: Workspace Entity 타입 수정

--- src/main/java/com/MindSpaceTeam/MindSpace/Entity/Workspace.java
+++ src/main/java/com/MindSpaceTeam/MindSpace/Entity/Workspace.java
@@ -1,21 +1,30 @@
 package com.MindSpaceTeam.MindSpace.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
 
 @Entity
 @Getter
 public class Workspace {
     @Id
     @GeneratedValue
+    @Column(name="workspaceId")
     private long workspaceId;
+    @Column(name="title")
     private String title;
-    private long createdAt;
+    @CreationTimestamp
+    @Column(name="createdAt", updatable = false)
+    private Instant createdAt;
 
     public Workspace(String title) {
         this.title = title;
     }
+
+    public Workspace() { }
 }
