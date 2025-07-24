54d16a7f5a582290bc5c83f53cb4f9792e1e074e
Younggwan <kimyogun78@naver.com>
2025-07-24 17:20:18 +0900

    feat: UserWorkspace테이블 접근 Repository정의

---
+++ src/main/java/com/MindSpaceTeam/MindSpace/Repository/UserWorkspaceRepository.java
@@ -0,0 +1,7 @@
+package com.MindSpaceTeam.MindSpace.Repository;
+
+import com.MindSpaceTeam.MindSpace.Entity.UserWorkspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserWorkspaceRepository extends JpaRepository<UserWorkspace, Long> {
+}
